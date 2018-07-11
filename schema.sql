a5fef66712d416644ab805d9ffb330613428d4e6
StewartBC <stewcapps@gmail.com>
2018-07-11 13:43:01 -0400

    added dummy data to schema

--- schema.sql
+++ schema.sql
@@ -9,5 +9,16 @@ CREATE TABLE products(
   department_name VARCHAR(100) NOT NULL,
   price DECIMAL(8,2) NOT NULL,
   stock_quantity INT(10) NOT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (item_id)
 );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Shampoo", "Home", 35, 4.50);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Laptop", "Electronics", 6, 750.99);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Keyboard", "Electronics", 13, 32.50);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Rug", "Home", 19, 25.00);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Shovel", "Outdoors", 9, 17.50);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Basketball", "Outdoors", 29, 19.99);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Dish Soap", "Home", 40, 6.75);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Hat", "Clothing", 21, 15.99);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Sneakers", "Clothing", 13, 39.99);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Phone Charger", "Electronics", 25, 9.99);
